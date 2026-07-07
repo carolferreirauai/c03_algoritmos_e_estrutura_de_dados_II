4c70c10ad83f2cd6872c5a628cd9136db74d3cc5
Caroline Ferreira de Souza <carolferreira.cf699@gmail.com>
2026-07-07 19:49:25 -0300

    feat - add ex de somar os nós de lista ligada

--- Moodle/listas_ligadas/list_soma_dos_nos.cpp
+++ Moodle/listas_ligadas/list_soma_dos_nos.cpp
@@ -14,3 +14,52 @@ PELO NÚMERO 0, QUE NÃO DEVE SER INSERIDO NA LISTA.
 
 SAÍDA: NA SAÍDA, O PROGRAMA DEVE MOSTRAR A SOMA DOS NÓS QUE FORAM INSERIDOS NA LISTA.
 */
+//biblioteca
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+//função soma
+int soma(list<int> lista)
+{
+    list<int>::iterator p; //ponteiro
+    int soma = 0;
+
+    //lista ligada - varre do inicio até o fim
+    for (p = lista.begin(); p != lista.end(); p++)
+    {
+        //soma da lista
+        soma += *p;
+    }
+    return soma;
+}
+
+//função principal
+int main()
+{
+    //declaração de variáveis
+    int num;
+    list<int> lista;
+    int total;
+
+    //entrada de dados
+    //ler o vários números e diferente de 0 e inseri-los em uma lista
+    while (cin >> num && num != 0)
+    {
+        lista.push_back(num);
+    }
+
+    //desenvolvimento
+    total = soma(lista);
+
+    //saída de dados
+    cout << total << endl;
+
+    //liberar a memória
+    while(!lista.empty())
+    {
+        lista.pop_front();
+    }
+    return 0;
+}

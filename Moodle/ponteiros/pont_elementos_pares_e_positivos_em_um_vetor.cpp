d4f18fd69d5844508c471e475c3e4fc9923433dc
Caroline Ferreira de Souza <carolferreira.cf699@gmail.com>
2026-07-07 13:02:00 -0300

    feat - add o ex de pares e positivos de ponteiros

--- Moodle/ponteiros/pont_elementos_pares_e_positivos_em_um_vetor.cpp
+++ Moodle/ponteiros/pont_elementos_pares_e_positivos_em_um_vetor.cpp
@@ -11,5 +11,52 @@ ENTRADA: A ENTRADA CONSISTE DE 2 LINHAS:
 - A PRIMEIRA LINHA CONTÉM APENAS UM INTEIRO N, QUE CORRESPONDE AO TAMANHO DO VETOR.
 - NA SEGUNDA LINHA, TEM-SE N INTEIROS, SEPARADOS POR UM ESPAÇO EM BRANCO.
 
-SAÍDA:
+SAÍDA: NA SAÍDA, O PROGRAMA DEVE MOSTRAR O NÚMERO DE ELEMENTOS PARES E POSITIVOS INSERIDOS NO VETOR.
 */
+//biblioteca
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //declaração de variáveis
+    int tamanho;
+    int *vetor;
+    int *p; //ponteiro auxiliar
+    int pares_e_positivos = 0; //contador de pares e positivos
+
+    //entrada de dados
+    cin >> tamanho; //ler o tamanho n de um vetor
+
+    vetor = new int[tamanho]; //alocar memória dinamicamente
+
+    //desenvolvimento
+    //inseri-los em um vetor
+    for (int i = 0; i < tamanho; i++)
+    {
+        cin >> vetor[i];
+    }
+
+    //ponteiro auxiliar aponta o inicio
+    p = vetor;
+    
+    //varrar o vetor
+    for (int i = 0; i < tamanho; i++)
+    {
+        //determinar os elementos pares e positivos
+        if (*p % 2 == 0 && *p > 0)
+        {
+            pares_e_positivos++;
+        }
+        p++; //avança para o próximo ponteiro
+    }
+
+    //saída de dados
+    cout << pares_e_positivos << endl;
+
+    //liberar memória
+    delete[] vetor;
+
+    return 0;
+}

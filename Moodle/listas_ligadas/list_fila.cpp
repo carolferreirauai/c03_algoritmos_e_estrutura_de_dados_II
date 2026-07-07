94005682c8f4dc2a112c42c8f05aba9ab7fb4007
Caroline Ferreira de Souza <carolferreira.cf699@gmail.com>
2026-07-07 17:05:25 -0300

    feat - add de fila em lista ligada

--- Moodle/listas_ligadas/list_fila.cpp
+++ Moodle/listas_ligadas/list_fila.cpp
@@ -9,3 +9,35 @@ DEPOIS O PROGRAMA DEVE IR REMOVENDO OS ELEMENTOS DA LISTA E MOSTRANDO-OS NA TELA
 ENTRADA: A ENTRADA CONSISTE DE 4 NÚMEROS INTEIROS
 SAÍDA: NA SAÍDA, O PROGRAMA DEVE MOSTRAR OS ELEMENTOS INSERIDOS.
 */
+//biblioteca
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+//função principal
+int main()
+{
+    //declaração de variaveis
+    int valores;
+    list<int> fila;
+
+    //entrada de dados
+    //ler 4 valores inteiros e inserir em uma lista
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> valores;
+        fila.push_back(valores);
+    }
+
+    //desenvolvimento e saída de dados
+    //o laço continua rodando enquanto a fila não estiver vazia
+    while(!fila.empty())
+    {
+        valores = *fila.begin(); //ler o inicio da fila
+        cout << valores << " "; //mostrando na tela
+        fila.pop_front(); //removendo da tela
+    }
+    cout << endl;
+    return 0;
+}

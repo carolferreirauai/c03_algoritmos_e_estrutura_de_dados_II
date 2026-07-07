f813ea37702ff5a3d3214a826f96eadfc8d2f4b7
Caroline Ferreira de Souza <carolferreira.cf699@gmail.com>
2026-07-07 20:03:01 -0300

    feat - add ex contar de nos de lista ligada

--- Moodle/listas_ligadas/list_contar_nos.cpp
+++ Moodle/listas_ligadas/list_contar_nos.cpp
@@ -14,3 +14,51 @@ NÚMERO 0, QUE NÃO DEVE SER INSERIDO NA LISTA.
 
 SAÍDA: NA SAÍDA, O PROGRAMA DEVE MOSTRAR O NÚMERO DE NÓS QUE FORAM INSERIDOS NA LISTA.
 */
+//biblioteca
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+//função contar
+int contar(list<int> lista)
+{
+    list<int>::iterator p; //ponteiro
+    int contador = 0;
+
+    for(p = lista.begin(); p != lista.end(); p++)
+    {
+        contador++;
+    }
+    return contador;
+}
+
+//função principal
+int main()
+{
+    //declaração de váriaveis
+    int num;
+    list<int> lista;
+    int nos;
+
+    //entrada de dados
+    //ler vários números e diferente de 0 e inseri-los na lista
+    while(cin >> num && num != 0)
+    {
+        lista.push_back(num);
+    }
+
+    //desenvolvimento
+    //chamar a função
+    nos = contar(lista);
+
+    //saida de dados
+    cout << nos << endl;
+
+    //liberar memória
+    while(!lista.empty())
+    {
+        lista.pop_front();
+    }
+    return 0;
+}
